dff4b863da16665dbb913f683bd0d616c9bc9490
Vinnie Falco <vinnie.falco@gmail.com>
2012-04-14 17:57:31 -0700

    Add copyright and license notices to template

--- templates/TagLibAmalgamTemplate.cpp
+++ templates/TagLibAmalgamTemplate.cpp
@@ -1,3 +1,24 @@
+/*
+  ==============================================================================
+
+  Copyright (c) 2002 Scott Wheeler, Lukas Lalinsky, Ismael Orenstein,
+  Allan Sandfeld Jensen, Teemu Tervo, Mathias Panzenbock
+
+  Official Project Location: http://developer.kde.org/~wheeler/taglib.html
+  Amalgamated Project Location: https://github.com/vinniefalco/TagLibAmalgam
+
+  ------------------------------------------------------------------------------
+
+  TagLib is distributed under the GNU Lesser General Public License (LGPL) and
+  Mozilla Public License (MPL). Essentially that means that it may be used in
+  proprietary applications, but if changes are made to TagLib they must be
+  contributed back to the project. Please review the licenses if you are
+  considering using TagLib in your project. See COPYING.MPL and COPYING.GPL for
+  more information on the licenses.
+
+  ==============================================================================
+*/
+
 #define TAGLIB_NO_CONFIG
 #define MAKE_TAGLIB_LIB
 #define TAGLIB_STATIC
@@ -138,3 +159,4 @@
 #pragma warning (pop)
 #pragma pop_macro ("_CRT_SECURE_NO_WARNINGS")
 #endif
+
